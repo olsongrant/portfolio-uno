e87113fa344b90e0e9af94dc11f93f67873dea4e
olsongrant <grant.olson@gmail.com>
2020-12-07 13:43:35 -0600

    initial commit

---
+++ my-portfolio/src/test/java/biz/mechanized/second/portfolio/MyPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.mechanized.second.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
